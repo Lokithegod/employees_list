c246aea3a365f9abe4b9c7d5aeda2c8ced429e2b
COMP086C\Hexxer <kss95kss@gmail.com>
2020-10-02 18:33:56 +0300

    change fot build it in to war

--- src/main/java/com/ve3yn4uk/employees_list/EmployeesListApplication.java
+++ src/main/java/com/ve3yn4uk/employees_list/EmployeesListApplication.java
@@ -2,11 +2,12 @@ package com.ve3yn4uk.employees_list;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 
 @SpringBootApplication
-public class EmployeesListApplication {
+public class EmployeesListApplication extends WebMvcConfigurationSupport {
 
-    public static void main(String[] args) {
+    public static void main(String[] args)  {
         SpringApplication.run(EmployeesListApplication.class, args);
     }
 
