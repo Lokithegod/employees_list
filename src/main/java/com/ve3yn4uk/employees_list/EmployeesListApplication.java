61df9b3a8707d3add677bd7b1c5304cc49ac4159
COMP086C\Hexxer <kss95kss@gmail.com>
2020-10-14 15:18:36 +0300

    commit from sublime merge

--- src/main/java/com/ve3yn4uk/employees_list/EmployeesListApplication.java
+++ src/main/java/com/ve3yn4uk/employees_list/EmployeesListApplication.java
@@ -2,12 +2,12 @@ package com.ve3yn4uk.employees_list;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class EmployeesListApplication extends WebMvcConfigurationSupport {
+public class EmployeesListApplication extends SpringBootServletInitializer {
 
-    public static void main(String[] args)  {
+    public static void main(String[] args) {
         SpringApplication.run(EmployeesListApplication.class, args);
     }
 
